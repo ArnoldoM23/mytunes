b824ed5e5b6c5aaf895fbbdbbe6498e950d50ce4
ArnoldoM23 <arnoldomunoz23@yahoo.com>
2016-07-05 18:42:51 -0700

    started mytunes

--- client/collections/SongQueue.js
+++ client/collections/SongQueue.js
@@ -2,6 +2,14 @@
 var SongQueue = Songs.extend({
 
   initialize: function(){
+    if (this.length <= 1) {
+      this.playFirst()
+    };
+  },
+  playFirst: function(){
+    console.log(this)
+    this.at(0).play();
   }
+
 
 });
